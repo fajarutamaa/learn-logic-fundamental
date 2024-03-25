927c5a3b696bf9de270f48d8e712fce1e778980d
fajarutamaa <fajardwiutomo75@gmail.com>
2024-03-25 21:47:46 +0700

    chore: update findPrime.js

--- javascript/easy/findPrimes.js
+++ javascript/easy/findPrimes.js
@@ -1,9 +1,19 @@
+// Program sederhana untuk mencari bilangan prima ke-n
+
 const findPrimes = (num) => {
     const isPrime = []
     const result = []
 
     for (let i = 2; i <= num; i++) {
-        if (!isPrime[i]
+        if (!isPrime[i]) {
+            result.push(i)
+            for (let j = i; j <= num; j += i) {
+                isPrime[j] = true
+            }
         }
     }
+    return result
 }
+
+
+console.log(findPrimes(50))
